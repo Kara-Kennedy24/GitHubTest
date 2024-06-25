08ab6ea21997235a001436010b10724bfab0fa05
Kara-Kennedy24 <kakenn54@gmail.com>
2024-06-25 10:04:02 -0700

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
